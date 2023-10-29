239e3bbeccf14b87072d0eed53a35250d92537bb
Windows81 <nastyahmede@gmail.com>
2023-10-29 06:21:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# myNoise: Static Version
+
+Still a work in progress. The source code extracted from the website was very messy and still needs lots of refactoring.
---
+++ Resources/in.php
@@ -0,0 +1,7 @@
+if( window._cgen ) {
+if( !_cgen_custom.timeout ) _cgen_custom.timeout = 240;
+if( !_cgen_custom.cookies_disable ) _cgen_custom.cookies_disable = 1;
+_cgen.advanced();
+_cgen.ping_start();
+_heatmaps_g2g_101364158='yes';
+}
